1a93732029857e39a1a0864e60785d400b443ca0
Girish V B <girishvb26@gmail.com>
2025-05-19 18:34:06 +0530

    Updated smartpointers,operator overload

--- SmartPointers/main.cpp
+++ SmartPointers/main.cpp
@@ -7,8 +7,12 @@ using namespace std;
 int main()
 {
 
-    // std::unique_ptr<Entity> entity = make_unique<Entity>();
-    // entity->print();
+    std::unique_ptr<Entity> entity0 = make_unique<Entity>();
+    entity0->print();
+
+    std::unique_ptr<Entity> entity1 = std::move(entity0); // Transfer ownership from entity0 to entity1
+    // entity0 is now empty
+    entity1->print();
 
     std::shared_ptr<Entity> shared1;
     {
